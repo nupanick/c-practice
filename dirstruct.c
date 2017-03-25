7700b8236378cd8b49b00a0c6c242e3956feedd2
Nicholas Lamicela <nupanick@gmail.com>
2017-03-25 10:44:36 -0400

    Finished up through exercise 1.5.

---
+++ dirstruct.c
@@ -0,0 +1,9 @@
+/* dirstruct.c
+ * Exercise 1.5: Use the \\ escape to print a file path.
+ * By Nicholas Lamicela, 2017
+ */
+#include <stdio.h>
+
+int main(){
+	printf("c:\\cygwin\\home\\administrator");
+}
---
+++ quote.c
@@ -0,0 +1,16 @@
+/* quote.c
+ * by Nicholas Lamicela, 2017
+ *
+ * Exercise 1.4
+ * Prints a quote.
+ */
+
+#include <stdio.h>
+
+main() {
+	printf("\"Sufficiently advanced technology is indistinguishable ");
+	printf("from magic.\"\n");
+	printf("\t--Arthur C. Clarke");
+	
+}
+		
