9eb2371b7a18b95a61fb6cf1c724a1948636fc05
Nicholas Lamicela <nupanick@gmail.com>
2017-03-25 11:02:03 -0400

    Exercise 1.7.

---
+++ calendar.c
@@ -0,0 +1,16 @@
+/* calendar.c
+ * by Nicholas Lamicela, March 2017
+ * Exercise 1.7.
+ * Prints out a map of the current month.
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("\nMon\tTue\tWed\tThu\tFri\tSat\tSun\n");
+	printf("\t\t 1\t 2\t 3\t 4\t 5\n");
+	printf(" 6\t 7\t 8\t 9\t10\t11\t12\n");
+	printf("13\t14\t15\t16\t17\t18\t19\n");
+	printf("20\t21\t22\t23\t24\t25\t26\n");
+	printf("27\t28\t29\t30\t31\n");
+}
