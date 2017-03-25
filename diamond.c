d4e3e5fdb3c62cfde51a8f8f69d19b6796e213f8
Nicholas Lamicela <nupanick@gmail.com>
2017-03-25 10:53:52 -0400

    Exercise 1.6

---
+++ diamond.c
@@ -0,0 +1,17 @@
+/* diamond.c
+ * Exercise 1.6
+ * Prints out a diamond shape.
+ * By Nicholas Lamicela, 2017.
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("      *\n");
+	printf("    *   *\n");
+	printf("  *       *\n");
+	printf("*           *\n");
+	printf("  *       *\n");
+	printf("    *   *\n");
+	printf("      *\n");
+}
